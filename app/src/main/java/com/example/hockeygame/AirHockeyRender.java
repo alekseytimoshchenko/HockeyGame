a714e31f4a2a94eb4ca7b00dc958ff61da61880a
Aleksey <alex.timowenko@gmail.com>
2019-07-04 11:26:08 +0300

    added opengl impl

--- app/src/main/java/com/example/hockeygame/AirHockeyRender.java
+++ app/src/main/java/com/example/hockeygame/AirHockeyRender.java
@@ -3,6 +3,7 @@ package com.example.hockeygame;
 import android.content.Context;
 import android.opengl.GLSurfaceView;
 
+import com.example.hockeygame.util.LoggerConfig;
 import com.example.hockeygame.util.ShaderHelper;
 import com.example.hockeygame.util.TextResourceReader;
 
@@ -11,8 +12,19 @@ import java.nio.ByteOrder;
 import java.nio.FloatBuffer;
 
 import static android.opengl.GLES20.GL_COLOR_BUFFER_BIT;
+import static android.opengl.GLES20.GL_FLOAT;
+import static android.opengl.GLES20.GL_LINES;
+import static android.opengl.GLES20.GL_POINTS;
+import static android.opengl.GLES20.GL_TRIANGLES;
 import static android.opengl.GLES20.glClear;
 import static android.opengl.GLES20.glClearColor;
+import static android.opengl.GLES20.glDrawArrays;
+import static android.opengl.GLES20.glEnableVertexAttribArray;
+import static android.opengl.GLES20.glGetAttribLocation;
+import static android.opengl.GLES20.glGetUniformLocation;
+import static android.opengl.GLES20.glUniform4f;
+import static android.opengl.GLES20.glUseProgram;
+import static android.opengl.GLES20.glVertexAttribPointer;
 import static android.opengl.GLES20.glViewport;
 
 import javax.microedition.khronos.egl.EGLConfig;
@@ -26,31 +38,39 @@ class AirHockeyRender implements GLSurfaceView.Renderer
 	private static final int BYTES_PER_FLOAT = 4;
 	private final FloatBuffer mVertexData;
 	private Context mContext;
+	private int mProgram;
 	
-	float[] mTableVerticesWithTriangles = { //
+	private static final String U_COLOR = "u_Color";
+	private int mUColorLocation;
+	
+	private static final String A_POSITION = "a_Position";
+	private int mAPositionLocation;
+	
+	private float[] mTableVerticesWithTriangles = { //
 			// Triangle 1
-			0f, 0f, //
-			9f, 14f, //
-			0f, 14f,//
+			- 0.5f, -0.5f, //
+			0.5f, 0.5f, //
+			-0.5f, 0.5f,
+			
 			// Triangle 2
-			0f, 0f, //
-			9f, 0f, //
-			9f, 14f,//
+			-0.5f, -0.5f,//
+			0.5f, -0.5f, //
+			0.5f, 0.5f,//
 			
 			// Line 1
-			0f, 7f, //
-			9f, 7f,//
+			-0.5f, 0f, //
+			0.5f, 0f,//
 			
 			// Mallets
-			4.5f, 2f,//
-			4.5f, 12f//
+			0f, -0.25f, //
+			0f, 0.25f//
 	};
 	
 	AirHockeyRender(@NonNull final Context iContext)
 	{
 		mContext = iContext;
 		
-		mVertexData = ByteBuffer.allocate(mTableVerticesWithTriangles.length * BYTES_PER_FLOAT)//
+		mVertexData = ByteBuffer.allocateDirect(mTableVerticesWithTriangles.length * BYTES_PER_FLOAT)//
 		                        .order(ByteOrder.nativeOrder())//
 		                        .asFloatBuffer();
 		
@@ -60,7 +80,7 @@ class AirHockeyRender implements GLSurfaceView.Renderer
 	@Override
 	public void onSurfaceCreated(final GL10 iGL10, final EGLConfig iEGLConfig)
 	{
-		glClearColor(1.0f, 0.0f, 0.0f, 0.0f);
+		glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
 		
 		final String vertexShaderSource = TextResourceReader.readTextFileFromResource(mContext, R.raw.simple_vertex_shader);
 		final String fragmentShaderSource = TextResourceReader.readTextFileFromResource(mContext, R.raw.simple_fragment_shader);
@@ -68,7 +88,30 @@ class AirHockeyRender implements GLSurfaceView.Renderer
 		final int vertexShader = ShaderHelper.compileVertexShader(vertexShaderSource);
 		final int fragmentShader = ShaderHelper.compileFragmentShader(fragmentShaderSource);
 		
+		mProgram = ShaderHelper.linkProgram(vertexShader, fragmentShader);
 		
+		if (LoggerConfig.ON)
+		{
+			ShaderHelper.validateProgram(mProgram);
+		}
+		
+		glUseProgram(mProgram);
+		getLocations();
+		bindData();
+	}
+	
+	private void bindData()
+	{
+		mVertexData.position(0);
+		glVertexAttribPointer(mAPositionLocation, POSITION_COMPONENT_COUNT, GL_FLOAT, false, 0, mVertexData);
+		glEnableVertexAttribArray(mAPositionLocation);
+		
+	}
+	
+	private void getLocations()
+	{
+		mUColorLocation = glGetUniformLocation(mProgram, U_COLOR);
+		mAPositionLocation = glGetAttribLocation(mProgram, A_POSITION);
 	}
 	
 	@Override
@@ -83,5 +126,21 @@ class AirHockeyRender implements GLSurfaceView.Renderer
 	{
 		//Clear the rendering surface
 		glClear(GL_COLOR_BUFFER_BIT);
+		
+		//Table
+		glUniform4f(mUColorLocation, 1.0f, 1.0f, 1.0f, 1.0f);
+		glDrawArrays(GL_TRIANGLES, 0, 6);
+		
+		//Middle line
+		glUniform4f(mUColorLocation, 1.0f, 0.0f, 0.0f, 1.0f);
+		glDrawArrays(GL_LINES, 6, 2);
+		
+		// Draw the first mallet blue.
+		glUniform4f(mUColorLocation, 0.0f, 0.0f, 1.0f, 1.0f);
+		glDrawArrays(GL_POINTS, 8, 1);
+		
+		// Draw the second mallet red.
+		glUniform4f(mUColorLocation, 1.0f, 0.0f, 0.0f, 1.0f);
+		glDrawArrays(GL_POINTS, 9, 1);
 	}
 }
--- app/src/main/java/com/example/hockeygame/util/ShaderHelper.java
+++ app/src/main/java/com/example/hockeygame/util/ShaderHelper.java
@@ -6,13 +6,22 @@ import androidx.annotation.NonNull;
 
 import static android.opengl.GLES20.GL_COMPILE_STATUS;
 import static android.opengl.GLES20.GL_FRAGMENT_SHADER;
+import static android.opengl.GLES20.GL_LINK_STATUS;
+import static android.opengl.GLES20.GL_VALIDATE_STATUS;
 import static android.opengl.GLES20.GL_VERTEX_SHADER;
+import static android.opengl.GLES20.glAttachShader;
 import static android.opengl.GLES20.glCompileShader;
+import static android.opengl.GLES20.glCreateProgram;
 import static android.opengl.GLES20.glCreateShader;
+import static android.opengl.GLES20.glDeleteProgram;
 import static android.opengl.GLES20.glDeleteShader;
+import static android.opengl.GLES20.glGetProgramInfoLog;
+import static android.opengl.GLES20.glGetProgramiv;
 import static android.opengl.GLES20.glGetShaderInfoLog;
 import static android.opengl.GLES20.glGetShaderiv;
+import static android.opengl.GLES20.glLinkProgram;
 import static android.opengl.GLES20.glShaderSource;
+import static android.opengl.GLES20.glValidateProgram;
 
 public class ShaderHelper
 {
@@ -68,5 +77,58 @@ public class ShaderHelper
 		}
 		
 		return shaderObjectId;
+	}
+	
+	public static int linkProgram(int vertexShaderId, int fragmentShaderId)
+	{
+		final int programObjectId = glCreateProgram();
+		
+		if (programObjectId == 0)
+		{
+			if (LoggerConfig.ON)
+			{
+				Log.w(TAG, "Could not create new program");
+			}
+			return 0;
+		}
+		
+		glAttachShader(programObjectId, vertexShaderId);
+		glAttachShader(programObjectId, fragmentShaderId);
+		
+		glLinkProgram(programObjectId);
+		
+		final int[] linkStatus = new int[1];
+		glGetProgramiv(programObjectId, GL_LINK_STATUS, linkStatus, 0);
+		
+		if (LoggerConfig.ON)
+		{
+			// Print the program info log to the Android log output.
+			Log.v(TAG, "Results of linking program:\n" + glGetProgramInfoLog(programObjectId));
+		}
+		
+		if (linkStatus[0] == 0)
+		{
+			// If it failed, delete the program object.
+			glDeleteProgram(programObjectId);
+			
+			if (LoggerConfig.ON)
+			{
+				Log.w(TAG, "Linking of program failed.");
+			}
+			
+			return 0;
+		}
+		
+		return programObjectId;
+	}
+	
+	public static boolean validateProgram(int programObjectId)
+	{
+		glValidateProgram(programObjectId);
+		final int[] validateStatus = new int[1];
+		glGetProgramiv(programObjectId, GL_VALIDATE_STATUS, validateStatus, 0);
+		Log.v(TAG, "Results of validating program: " + validateStatus[0] + "\nLog:" + glGetProgramInfoLog(programObjectId));
+		
+		return validateStatus[0] != 0;
 	}
 }

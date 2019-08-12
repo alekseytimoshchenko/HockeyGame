e398e7cea0e82a7c79e99acfe07216c61ce43406
Aleksey <alex.timowenko@gmail.com>
2019-08-12 10:23:34 +0300

    added convert from 2D to 3d click

--- app/src/main/java/com/example/hockeygame/AirHockeyActivity.java
+++ app/src/main/java/com/example/hockeygame/AirHockeyActivity.java
@@ -2,12 +2,15 @@ package com.example.hockeygame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.app.ActivityManager;
 import android.content.Context;
 import android.content.pm.ConfigurationInfo;
 import android.opengl.GLSurfaceView;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.Toast;
 
 public class AirHockeyActivity extends AppCompatActivity
@@ -15,6 +18,7 @@ public class AirHockeyActivity extends AppCompatActivity
 	private GLSurfaceView mGLSurfaceView;
 	private boolean mRendererSet = false;
 	
+	@SuppressLint("ClickableViewAccessibility")
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
@@ -24,13 +28,15 @@ public class AirHockeyActivity extends AppCompatActivity
 		
 		boolean supportsEs2 = checkOpenGL20Compatibility();
 		
+		final AirHockeyRenderer airHockeyRenderer = new AirHockeyRenderer(this);
+		
 		if (supportsEs2)
 		{
 			//Request an OpenGL ES 2.0 compatible context.
 			mGLSurfaceView.setEGLContextClientVersion(2);
 			
 			//Assign our render
-			mGLSurfaceView.setRenderer(new AirHockeyRender(this));
+			mGLSurfaceView.setRenderer(airHockeyRenderer);
 			mRendererSet = true;
 		}
 		else
@@ -38,6 +44,51 @@ public class AirHockeyActivity extends AppCompatActivity
 			Toast.makeText(this, "This device does not support OpneGL ES 2.0", Toast.LENGTH_SHORT).show();
 			return;
 		}
+		
+		mGLSurfaceView.setOnTouchListener(new View.OnTouchListener()
+		{
+			@Override
+			public boolean onTouch(View v, MotionEvent event)
+			{
+				if (event != null)
+				{
+					// Convert touch coordinates into normalized device
+					// coordinates, keeping in mind that Android's Y
+					// coordinates are inverted.
+					final float normalizedX = (event.getX() / (float) v.getWidth()) * 2 - 1;
+					final float normalizedY = -((event.getY() / (float) v.getHeight()) * 2 - 1);
+					
+					if (event.getAction() == MotionEvent.ACTION_DOWN)
+					{
+						mGLSurfaceView.queueEvent(new Runnable()
+						{
+							@Override
+							public void run()
+							{
+								airHockeyRenderer.handleTouchPress(normalizedX, normalizedY);
+							}
+						});
+					}
+					else if (event.getAction() == MotionEvent.ACTION_MOVE)
+					{
+						mGLSurfaceView.queueEvent(new Runnable()
+						{
+							@Override
+							public void run()
+							{
+								airHockeyRenderer.handleTouchDrag(normalizedX, normalizedY);
+							}
+						});
+					}
+					
+					return true;
+				}
+				else
+				{
+					return false;
+				}
+			}
+		});
 		
 		setContentView(mGLSurfaceView);
 	}
@@ -63,6 +114,5 @@ public class AirHockeyActivity extends AppCompatActivity
 				&& (Build.FINGERPRINT.startsWith("generic") || Build.FINGERPRINT.startsWith("unknown") //
 				|| Build.MODEL.contains("google_sdk") || Build.MODEL.contains("Emulator") || Build.MODEL.contains("Android SDK built for x86"))//
 		);
-		
 	}
 }
--- app/src/main/java/com/example/hockeygame/util/Geometry.java
+++ app/src/main/java/com/example/hockeygame/util/Geometry.java
@@ -1,5 +1,7 @@
 package com.example.hockeygame.util;
 
+import java.util.Vector;
+
 public class Geometry
 {
 	public static class Point
@@ -34,6 +36,35 @@ public class Geometry
 		{
 			return new Circle(center, radius * scale);
 		}
+	}
+	
+	public static class Ray
+	{
+		public final Point point;
+		public final Vector vector;
+		
+		public Ray(Point point, Vector vector)
+		{
+			this.point = point;
+			this.vector = vector;
+		}
+	}
+	
+	public static class Vector
+	{
+		public final float x, y, z;
+		
+		public Vector(float x, float y, float z)
+		{
+			this.x = x;
+			this.y = y;
+			this.z = z;
+		}
+	}
+	
+	public static Vector vectorBetween(Point from, Point to)
+	{
+		return new Vector(to.x - from.x, to.y - from.y, to.z - from.z);
 	}
 	
 	public static class Cylinder
